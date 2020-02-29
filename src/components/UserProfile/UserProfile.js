bbd19857da04298b673862925388eae65df06934
jlatour88 <jlatour88@gmail.com>
2020-02-29 17:03:09 -0600

    Changed from dynamic inputs to more traditional inputs

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,44 +1,56 @@
 import React from 'react';
+import UserFields from './InputFields/UserFields';
+import PhysicianFields from './InputFields/PhysicianFields';
+import SpouseFields from './InputFields/SpouseFields';
 import { Formik } from 'formik';
-import { TextField, Button } from '@material-ui/core';
-import userInputs from './inputs';
-import styles from './UserProfile.module.css';
+import { Button } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
 
 const UserProfile = (props) => {
+  const classes = useStyles();
   return (
     <div>
       <h2> Hello User</h2>
+      <Formik initialValues={defaultValues} onSubmit={data => { console.log(data); }}>
+        {({ values, handleChange, handleSubmit }) => (
+          <form className={classes.root} onSubmit={handleSubmit}>
+            {/* //? Fields including User, Physician, & Spouse */}
+            <UserFields values={values} handleChange={handleChange} />
+            <hr />
+            <PhysicianFields values={values} handleChange={handleChange} />
+            <hr />
+            <SpouseFields values={values} handleChange={handleChange} />
 
-      <Formik
-        initialValues={{}}
-        onSubmit={data => {
-          console.log(data);
-        }}
-      >
-        {({ values, handleChange, handleBlur, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            {userInputs.map(input =>
-              <div key={input.name} className={styles[input.name]}>
-                <TextField
-                  type={input.type}
-                  name={input.name}
-                  // label={input.placeholder}
-                  placeholder={input.placeholder}
-                  value={values[input.name]}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  variant='outlined'
-                />
-              </div>
-            )}
-            <br /> {/* Usage Temporary */}
-            <Button variant='contained' color='primary'>Submit</Button>
-            <pre>{JSON.stringify(values, null, 2)}</pre>
+            {/* //? Submit Button */}
+            <Button variant='contained' color='primary' type='submit'>Submit</Button>
+            <pre>{JSON.stringify(values, null, 2)}</pre> {/* //! Used for testing purposes */}
           </form>
         )}
       </Formik>
     </div>
   )
+}
+
+const defaultValues = {
+  userInfo: {
+    firstName: '', middleName: '', surName: '', dob: '', gender: '', phoneNo: '', email: '', local: ''
+  },
+  physicianInfo: {
+    id: '', firstName: '', middleName: '', surName: '', gender: '', phoneNo: '', email: ''
+  },
+  spouseInfo: {
+    id: '', firstName: '', middleName: '', surName: '', gender: '', dob: ''
+  }
 }
 
 export default UserProfile
