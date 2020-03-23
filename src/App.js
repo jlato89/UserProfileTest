e0f62eb1a8d009f06255b06990bb15577c1b040d
Josh Latour <jlatour88@gmail.com>
2020-03-23 11:28:28 -0500

    Comment out UserProfile component and added DynamicDropDown test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import UserProfile from './components/UserProfile/UserProfile';
+// import UserProfile from './components/UserProfile/UserProfile';
+import DynamicDropDown from './components/DynamicDropDown/DynamicDropDown';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <UserProfile />
+      {/* <UserProfile /> */}
+      <DynamicDropDown />
     </div>
   );
 }
